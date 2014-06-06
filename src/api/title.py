9b58687d6efe988f0b9209e662e468575f70e393
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-06-06 10:56:32 +0300

    Added equality check for the Title object.

--- src/api/title.py
+++ src/api/title.py
@@ -6,6 +6,19 @@ class Title(object):
         self.imdb_id    = imdb_id
         from namenormalization import normalize_name
         self.normalized_names = normalize_name(self.name)
+        self.normalized_names_set = set(self.normalized_names)
+
+    def __eq__(self, other):
+        # When imdb_id is not empty, and equals.
+        if self.imdb_id and self.imdb_id == other.imdb_id:
+            return True
+        # Even if the year is empty.
+        elif self.year == other.year:
+            intersection = self.normalized_names_set.intersection(
+                other.normalized_names_set)
+            if intersection:
+                return True
+        return False
         
 class MovieTitle(Title):
     """ Title object for movies. """
@@ -23,3 +36,4 @@ class SeriesTitle(Title):
         self.episode_number = episode_number
         from namenormalization import normalize_name
         self.episode_normalized_names = normalize_name(self.episode_name)
+        self.episode_normalized_names_set = set(self.episode_normalized_names)
