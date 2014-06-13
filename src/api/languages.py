5009e69217ac9de7e9674c9e1be53df62331f385
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-06-13 17:14:56 +0300

    Added __iter__ function to the Languages class. This will allow use to receive all the languages that SubiT support simply by iterating the Languages class.

--- src/api/languages.py
+++ src/api/languages.py
@@ -6,6 +6,9 @@ class Languages(object):
 	This is the only place where the language is referenced directly, and not 
 	via its "Language" instance. In any other place, there will be a reference
 	to this place.
+
+	The class implements the __iter__ function so it can be iterated in order
+	to get all the languages that it holds.
 	"""
 	class Language(object):
 		def __init__(self, full_name, iso_name):
@@ -19,5 +22,16 @@ class Languages(object):
 			return "<Language full_name='%s', iso_name='%s'>" % \
 				(self.full_name, self.iso_name)
 
+	class __metaclass__(type):
+		""" 
+		Define a metaclass in here in order to implement the __iter__ function.
+		Otherwise, we won't be able to use __iter__ on the class object itself,
+		and not instance of it.
+		"""
+		def __iter__(self):
+			for name, value in Languages.__dict__.items():
+				if isinstance(value, Languages.Language):
+					yield value
 	
 	HEBREW = Language("Hebrew", "heb")
+
