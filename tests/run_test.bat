c7152102a5b5ca8822422eebd64f3c01f54e8fb0
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-06-13 04:36:09 +0300

    Removed spaces from the set command in the run_test.bat script.

--- tests/run_test.bat
+++ tests/run_test.bat
@@ -1,8 +1,8 @@
 :: Add the src directory to the python's path.
 if "%PYTHONPATH%" == "" (
-	set PYTHONPATH = %~dp0..\src
+	set PYTHONPATH=%~dp0..\src
 ) else (
-	set PYTHONPATH = %PYTHONPATH% + %~dp0..\src
+	set PYTHONPATH=%PYTHONPATH% + %~dp0..\src
 )
 
 :: Execute the script
