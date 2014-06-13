63de594f5c3e8611dd94b344c6b8bd5cafa7920e
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-06-13 03:38:15 +0300

    Added a batch script that receives a test file as input, and execute it while including the src directory in the PYTHONPATH.

---
+++ tests/run_test.bat
@@ -0,0 +1,9 @@
+:: Add the src directory to the python's path.
+if "%PYTHONPATH%" == "" (
+	set PYTHONPATH = %~dp0..\src
+) else (
+	set PYTHONPATH = %PYTHONPATH% + %~dp0..\src
+)
+
+:: Execute the script
+python %~f1
