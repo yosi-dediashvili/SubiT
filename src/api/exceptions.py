16d9f431b77cf014921439ea171a6784f10350ac
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-06-13 05:47:41 +0300

    Added doctest to the Version constructor.

--- src/api/exceptions.py
+++ src/api/exceptions.py
@@ -1,3 +1,6 @@
 class InvalidTitleName(Exception): pass
 class InvalidSeasonNumber(Exception): pass
 class InvalidEpisodeNumber(Exception): pass
+
+class InvalidTitleValue(Exception): pass
+class InvalidNumOfCDs(Exception): pass
--- src/api/version.py
+++ src/api/version.py
@@ -7,10 +7,35 @@ Users of this package will implement the methods for extracting the required
 info for the Version.
 """
 
-__all__ = ['Version', 'ProviderVersion']
+__all__ = ['Version', 'ProviderVersion', 'UKNOWN_NUM_OF_CDS']
+
+from exceptions import InvalidTitleValue
+from exceptions import InvalidNumOfCDs
+
+UKNOWN_NUM_OF_CDS = 0
 
 class Version(object):
-	def __init__(self, identifiers, title, num_of_cds = 0):
+	def __init__(self, identifiers, title, num_of_cds = UKNOWN_NUM_OF_CDS):
+		"""
+		A version is instantiated with an identifiers list that can be and 
+		empty list, a title that must be valid, and an optional num_of_cds
+		value that defaults to unknown, and cannot be lower than 0.
+
+		>>> Version(["720p", "dts", "lol"], None, 1)
+        Traceback (most recent call last):
+            ...
+        InvalidTitleValue: Title instance must be provided.
+
+        >>> from title import MovieTitle
+        >>> title = MovieTitle("The Matrix")
+		>>> Version(["720p", "dts", "lol"], title, -1)
+        Traceback (most recent call last):
+            ...
+        InvalidNumOfCDs: num_of_cds cannot be lower than 0.
+
+        >>> Version(["720p", "dts", "lol"], title, 1)
+        <Version ...>
+		"""
 		pass
 
 class ProviderVersion(Version):
