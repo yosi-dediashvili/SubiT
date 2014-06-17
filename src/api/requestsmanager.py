8b8d4932cc25283ca40dbfd1a4fcf3c29b5a42a1
Nimrod Goldrat <nimrod@goldrat.info>
2014-06-17 17:29:16 +0300

    initial version

--- src/api/requestsmanager.py
+++ src/api/requestsmanager.py
@@ -2,35 +2,44 @@ from exceptions import InvalidProviderName
 
 class RequestsManager(object):
 
-    def perform_request(domain, url, data, type, more_headers):
-    	raise NotImplementedError
+    def perform_request(self, domain, url, 
+        data = '', type = HttpRequestTypes.GET, more_headers = {}):
+        """
+        Locks the requests mutex, and after that, sends the request. If the 
+        mutex is already lock, the function will block until the mutex is 
+        released, and we manage to lock it.
+        """
+        raise NotImplementedError
 
-    def perform_request_next(domain, url, data, type, more_headers): 
-    	raise NotImplementedError
+    def perform_request_next(self, domain, url, 
+        data = '', type = HttpRequestTypes.GET, more_headers = {}):
+        """
+        Perform a request without locking the mutex.
+        """
+        raise NotImplementedError
 
-    _instances = {}
     @classmethod
     def get_instance(cls, provider_name):
-    	"""
-    	A RequestsManager factory that given the same provider name will return
-    	the same RequestsManager instance. 
+        """
+        A RequestsManager factory that given the same provider name will return
+        the same RequestsManager instance. 
 
-    	This currently isn't a thread safe factory, so in case where to 
-    	different threads trying to receive a RequestsManager for a 
-    	provider_name that hasn't already been initialzied, will get in to a 
-    	race condition, in which they might end up with different instances.
+        This currently isn't a thread safe factory, so in case where to 
+        different threads trying to receive a RequestsManager for a 
+        provider_name that hasn't already been initialzied, will get in to a 
+        race condition, in which they might end up with different instances.
 
-    	>>> a = RequestsManager.get_instance("a")
-    	>>> b = RequestsManager.get_instance("a")
-    	>>> id(a) == id(b)
-    	True
-    	>>> b = RequestsManager.get_instance("b")
-    	>>> id(b) == id(a)
-    	False
-    	>>> b = RequestsManager.get_instance("a")
-    	>>> id(a) == id(b)
-    	True
-    	>>> RequestsManager.get_instance("")
+        >>> a = RequestsManager.get_instance("a")
+        >>> b = RequestsManager.get_instance("a")
+        >>> id(a) == id(b)
+        True
+        >>> b = RequestsManager.get_instance("b")
+        >>> id(b) == id(a)
+        False
+        >>> b = RequestsManager.get_instance("a")
+        >>> id(a) == id(b)
+        True
+        >>> RequestsManager.get_instance("")
         Traceback (most recent call last):
             ...
         InvalidProviderName: provider_name can not be empty.
@@ -38,12 +47,5 @@ class RequestsManager(object):
         Traceback (most recent call last):
             ...
         InvalidProviderName: provider_name must be a string.
-    	"""
-    	if not provider_name:
-    		raise InvalidProviderName("provider_name can not be empty.")
-    	if not isinstance(provider_name, str):
-    		raise InvalidProviderName("provider_name must be a string.")
-
-    	if not provider_name in cls._instances:
-    		cls._instances[provider_name] = cls()
-    	return cls._instances[provider_name]
+        """
+        raise NotImplementedError
