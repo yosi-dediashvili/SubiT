365655e89b46a96b1467ff4ea374d5e2d2733b76
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-06-22 00:58:06 +0300

    Updated the run_test.py such that it now changes the CWD to the test path before executing it.

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,6 +1,6 @@
 import sys
 from os.path import *
-from os import getcwd
+from os import getcwd, chdir
 import argparse
 
 sys.path.append(join(abspath(dirname(__file__)), r"..\src"))
@@ -10,12 +10,24 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "test_paths", metavar='FILE', type=str, nargs="+", 
     help="Full or Relative (to the current directory) path to the test file")
-
 args = parser.parse_args()
+
+
+def run_test(test_path):
+    print "Running test file: %s" % test_path
+    global_wd = getcwd()
+    dir_name = dirname(test_path)
+    sys.path.append(dir_name)
+    chdir(dir_name)
+    execfile(test_path)
+    sys.path.remove(dir_name)
+    chdir(global_wd)
+
 for test_path in args.test_paths:
     if not isabs(test_path):
         test_path = abspath(join(getcwd(), test_path))
     if not exists(test_path):
         raise ValueError("The test file is missing: %s" % test_path)
-    print "Running test file: %s" % test_path
-    execfile(test_path)
+    
+    run_test(test_path)
+    
