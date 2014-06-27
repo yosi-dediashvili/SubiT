c01a13c299cdbf3e73ca12007e83a0b7eb66095c
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-06-27 05:10:13 +0300

    Added documentation to the discover_title function.

--- src/api/titlediscovery.py
+++ src/api/titlediscovery.py
@@ -2,4 +2,17 @@ __all__ = ['discover_title']
 
 
 def discover_title(query):
+    """
+    Tries to discover the title standing behind the query given. The query might
+    be just some string, or a full path to a movie/series files.
+
+    If the query is just a string, then we'll try only the simple method for the
+    extraction. That is, sending the query as-is to OpenSubtitles. Otherwise, if
+    it's a full path to a file, will use all the three methods (file name, hash 
+    and directory name).
+
+    If the function succeeds in discovering the title, a Title instance of the
+    appropriate kind is returned (Movie or Series Title). If not, None is 
+    returned.
+    """
     pass
