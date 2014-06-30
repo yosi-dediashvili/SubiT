07575a0a80db57fe068765971208a8be32ec055b
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-07-01 00:22:42 +0300

    Added hash check for the movie file test.

--- tests/api/test_titlediscovery.py
+++ tests/api/test_titlediscovery.py
@@ -1,6 +1,7 @@
 from api import titlediscovery
 
 import doctest
+from hashlib import sha1
 import unittest
 import os
 
@@ -10,14 +11,20 @@ PATH_TO_MOVIE_FILE = os.path.join(
     'Tears of Steel', 
     'Tears.of.Steel[SHORT].2012.BRRip.AC3.XViD-RemixHD.avi')
 
+MOVIE_FILE_SHA1 = "3f2b0a7ed4bca05a5e5aa9095368ef1b0d856861"
+
 class TestTitleDiscovery(unittest.TestCase):
     def test_file_hash(self):
         title = titlediscovery.discover_title(PATH_TO_MOVIE_FILE)
-        
+
         self.assertIsNotNone(title)
         self.assertEquals(title.name, "Tears of Steel")
         self.assertEquals(title.year, 2012)
         self.assertEquals(title.imdb_id, "tt2285752")
+
+def hash_matches():
+    sha1_val = sha1(open(PATH_TO_MOVIE_FILE, "rb").read()).hexdigest()
+    return sha1_val == MOVIE_FILE_SHA1
 
 def run_tests():
     doctest.testmod(
@@ -25,12 +32,13 @@ def run_tests():
         verbose=False, 
         optionflags=doctest.NORMALIZE_WHITESPACE)
 
-    if os.path.exists(PATH_TO_MOVIE_FILE):
+    if os.path.exists(PATH_TO_MOVIE_FILE) and hash_matches():
         unittest.TextTestRunner(verbosity=0).run(
-            unittest.defaultTestLoader.loadTestsFromTestCase(TestDiscoveryTest))
+            unittest.defaultTestLoader.loadTestsFromTestCase(
+                TestTitleDiscovery))
     else:
         print 
         print
-        print "------------------------------------------------------"
-        print "Not testing with real files. Movie is missing."
-        print "------------------------------------------------------"
+        print "---------------------------------------------------------------"
+        print "Not testing with files. Movie is missing (or incorrect hash).  "
+        print "---------------------------------------------------------------"
