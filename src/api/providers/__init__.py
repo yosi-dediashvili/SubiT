63742fba4460b0d127ae6d4e1e2b501228c0743d
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-07-04 03:52:27 +0300

    Updated the docs for the providers factory method.

--- src/api/providers/__init__.py
+++ src/api/providers/__init__.py
@@ -2,7 +2,9 @@ import logging
 logger = logging.getLogger("subit.api.providers")
 
 from api.exceptions import UnsupportedLanguage
+from api.exceptions import InvalidProviderName
 
+from api.requestsmanager import RequestsManager
 
 __all__ = ['get_titles_versions']
 
@@ -12,12 +14,14 @@ PROVIDERS = []
 
 
 def get_provider_instance(provider_name, languages, 
-    requests_manager_factory = None, providers = PROVIDERS):
+    requests_manager_factory = RequestsManager.get_instance, 
+    providers = PROVIDERS):
     """
-    Factory method for creating Provider instances. provider_name value should 
-    be one of the providers names stored in ProvidersNames, otherwise, exception
-    will be raised. languages should be a list of one or more Language instances
-    from the Languages class.
+    Factory method for creating Provider instances. The provider_name value 
+    should be a valid ProviderName instance. If none of the providers specified
+    in the providers list does not match the ProviderName, InvalidProviderName
+    exception is raised. languages should be a list of one or more Language 
+    instances from the Languages class.
 
     A RequestsManager factory method can be passed to the function in order to 
     bypass the default factory method. Also, a list of Provider classes can be
@@ -27,4 +31,6 @@ def get_provider_instance(provider_name, languages,
     If none of the languages specified is supported in the provider specified,
     an UnsupportedLanguage exception will be raised.
     """
-    pass
+
+
+    
