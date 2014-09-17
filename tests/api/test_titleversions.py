f8e5c81b70e87533285c0becad2230f199ea1a96
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-09-17 23:26:31 +0300

    Finished the tests implementation for the new design of the TitlesVersions.

--- tests/api/test_titleversions.py
+++ tests/api/test_titleversions.py
@@ -21,27 +21,28 @@ class TestTitleVersions(unittest.TestCase):
         self.titles_versions.add_version(heb_version)
         self.titles_versions.add_version(eng_version)
 
-        self.assertIn(Languages.HEBREW, self.titles_versions[0].versions.keys())
-        self.assertIn(Languages.ENGLISH, self.titles_versions[0].versions.keys())
+        self.assertIn(Languages.HEBREW, self.titles_versions[0][1].keys())
+        self.assertIn(Languages.ENGLISH, self.titles_versions[0][1].keys())
 
         # Check rank groups
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 heb_version.rank_group))
         self.assertTrue(
-            self.titles_versions.versions[Languages.ENGLISH].has_key(
+            self.titles_versions[0][1][Languages.ENGLISH].has_key(
                 eng_version.rank_group))
 
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [heb_version.rank_group][0][1],
             heb_version)
         self.assertEqual(
-            self.titles_versions.versions[Languages.ENGLISH]\
+            self.titles_versions[0][1][Languages.ENGLISH]\
                 [eng_version.rank_group][0][1],
             eng_version)
 
-    def test_same_language_different_rank_group(self):
+    def test_single_title_same_language_different_rank_group(self):
+        title = MovieTitle("The Matrix", 1999)
         a_version = ProviderVersion(
             [], self.title, Languages.HEBREW, MockedProvider(), rank=80)
         b_version = ProviderVersion(
@@ -52,22 +53,23 @@ class TestTitleVersions(unittest.TestCase):
 
         # Check rank groups
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 a_version.rank_group))
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 b_version.rank_group))
 
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [a_version.rank_group][0][1],
             a_version)
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [b_version.rank_group][0][1],
             b_version)                
 
-    def test_same_rank_group_different_rank(self):
+    def test_single_title_same_rank_group_different_rank(self):
+        title = MovieTitle("The Matrix", 1999)
         a_version = ProviderVersion(
             [], self.title, Languages.HEBREW, MockedProvider(), rank=82)
         b_version = ProviderVersion(
@@ -78,30 +80,31 @@ class TestTitleVersions(unittest.TestCase):
 
         # Check rank groups
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 a_version.rank_group))
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 b_version.rank_group))
 
         # Check that the group contain 2 versions.
         self.assertEqual(
-            len(self.titles_versions.versions[Languages.HEBREW]
+            len(self.titles_versions[0][1][Languages.HEBREW]
                 [a_version.rank_group]),
             2)
 
         # Check that the first position is held by the version with the higher
         # rank.
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [a_version.rank_group][0][1],
             a_version)
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [b_version.rank_group][1][1],
             b_version)       
 
-    def test_same_rank_different_provider_rank(self):
+    def test_single_title_same_rank_different_provider_rank(self):
+        title = MovieTitle("The Matrix", 1999)
         a_version = ProviderVersion(
             [], self.title, Languages.HEBREW, MockedProvider(), rank=80)
         b_version = ProviderVersion(
@@ -112,30 +115,31 @@ class TestTitleVersions(unittest.TestCase):
 
         # Check rank groups
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 a_version.rank_group))
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 b_version.rank_group))
 
         # Check that the group contain 2 versions.
         self.assertEqual(
-            len(self.titles_versions.versions[Languages.HEBREW]
+            len(self.titles_versions[0][1][Languages.HEBREW]
                 [a_version.rank_group]),
             2)
 
         # Check that the first position is held by the version with the higher
         # provider_rank.
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [a_version.rank_group][0][1],
             a_version)
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [b_version.rank_group][1][1],
             b_version)  
 
-    def test_opposite_provider_and_version_rank(self):
+    def test_single_title_opposite_provider_and_version_rank(self):
+        title = MovieTitle("The Matrix", 1999)
         a_version = ProviderVersion(
             [], self.title, Languages.HEBREW, MockedProvider(), rank=85)
         b_version = ProviderVersion(
@@ -146,29 +150,59 @@ class TestTitleVersions(unittest.TestCase):
 
         # Check rank groups
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 a_version.rank_group))
         self.assertTrue(
-            self.titles_versions.versions[Languages.HEBREW].has_key(
+            self.titles_versions[0][1][Languages.HEBREW].has_key(
                 b_version.rank_group))
 
         # Check that the group contain 2 versions.
         self.assertEqual(
-            len(self.titles_versions.versions[Languages.HEBREW]
+            len(self.titles_versions[0][1][Languages.HEBREW]
                 [a_version.rank_group]),
             2)
 
         # Check that the first position is held by the version with the higher
         # provider_rank.
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [a_version.rank_group][0][1],
             a_version)
         self.assertEqual(
-            self.titles_versions.versions[Languages.HEBREW]\
+            self.titles_versions[0][1][Languages.HEBREW]\
                 [b_version.rank_group][1][1],
             b_version) 
 
+    def test_two_titles(self):
+        title_a = MovieTitle("The Matrix Reloaded", 2003)
+        title_b = MovieTitle("The Green Mile", 1999)
+
+        a_version = ProviderVersion(
+            [], title_a, Languages.HEBREW, MockedProvider(), rank=85)
+        b_version = ProviderVersion(
+            [], title_b, Languages.HEBREW, MockedProvider(), rank=87)
+
+        self.titles_versions.add_version(a_version)
+        self.titles_versions.add_version(b_version)
+
+        # Check that we have two titles
+        self.assertEqual(len(self.titles_versions), 2)
+
+    def test_iteration(self):
+        versions = []
+        versions.append(ProviderVersion(
+            [], MovieTitle("The Matrix"), Languages.HEBREW, MockedProvider()))
+
+        versions.append(ProviderVersion(
+            [], MovieTitle("Titanic"), Languages.ENGLISH, MockedProvider()))
+
+        versions.append(ProviderVersion(
+            [], MovieTitle("Gladiator"), Languages.ENGLISH, MockedProvider()))
+
+        # Just make sure it works, and we got the titles.
+        titles_versions = TitlesVersions(versions)
+        for title, versions in titles_versions:
+            self.assertIn(title, versions)
 
 def run_tests():
     unittest.TextTestRunner(verbosity=0).run(
