7b0be5bd05f922ec3614908ac632bcbc91f3d743
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-10-08 00:05:24 +0300

    Added all the langauges that are about to be supported in SubiT.

--- src/api/languages.py
+++ src/api/languages.py
@@ -1,9 +1,9 @@
 class Languages(object):
 	"""
-	This class holds all the supported languages by SubiT. Each languages has 
+	This class holds all the supported languages by SubiT. Each languages has
 	its full name, and its short, ISO 639-2 version.
 
-	This is the only place where the language is referenced directly, and not 
+	This is the only place where the language is referenced directly, and not
 	via its "Language" instance. In any other place, there will be a reference
 	to this place.
 
@@ -17,7 +17,7 @@ class Languages(object):
 
 		def __eq__(self, other):
 			return (
-				self.full_name == other.full_name and 
+				self.full_name == other.full_name and
 				self.iso_name == other.iso_name)
 
 		def __str__(self):
@@ -26,11 +26,11 @@ class Languages(object):
 		def __repr__(self):
 			return (
 				"<Language full_name='%(full_name)s', "
-				"iso_name='%(iso_name)s'>" 
+				"iso_name='%(iso_name)s'>"
 				% self.__dict__)
 
 	class __metaclass__(type):
-		""" 
+		"""
 		Define a metaclass in here in order to implement the __iter__ function.
 		Otherwise, we won't be able to use __iter__ on the class object itself,
 		and not instance of it.
@@ -39,10 +39,10 @@ class Languages(object):
 			for name, value in Languages.__dict__.items():
 				if isinstance(value, Languages.Language):
 					yield value
-	
+
 	@staticmethod
 	def locate_language(language_string):
-		""" 
+		"""
 		Will try to retrieve a language instance based on the provided string
 		which might be either the iso code or the full language name. Returns
 		None when no such language exists in the list.
@@ -62,6 +62,15 @@ class Languages(object):
 				return language
 		return None
 
-	HEBREW = Language("Hebrew", "heb")
-	ENGLISH = Language("English", "eng")
-
+	HEBREW 		= Language("Hebrew", 	"heb")
+	ENGLISH 	= Language("English", 	"eng")
+	SPANISH 	= Language("Spanish", 	"spa")
+	ARABIC 		= Language("Arabic", 	"ara")
+	BULGARIAN 	= Language("Bulgarian", "bul")
+	SLOVAK 		= Language("Slovak", 	"slo")
+	TURKISH 	= Language("Turkish", 	"tur")
+	CZECH 		= Language("Czech", 	"cze")
+	RUSSIAN 	= Language("Russian", 	"rus")
+	NORWEGIAN 	= Language("Norwegian", "nor")
+	SWEDISH 	= Language("Swedish", 	"swe")
+	FRENCH 		= Language("French", 	"fre")
