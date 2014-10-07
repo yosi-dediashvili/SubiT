d3e7acc49dcc221654c929fea54e33e892a2a172
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-10-08 01:32:54 +0300

    Implemented a single test for Addic7ed.

--- src/api/providers/addic7ed/provider.py
+++ src/api/providers/addic7ed/provider.py
@@ -1,9 +1,31 @@
 import logging
 logger = logging.getLogger("subit.api.providers.addic7ed.provider")
 
-from api.providers import IProvider
+from api.providers.iprovider import IProvider
+from api.providers.providersnames import ProvidersNames
+from api.languages import Languages
+
 
 __all__ = ['Addic7edProvider']
 
+
 class Addic7edProvider(IProvider):
-    pass
+    provider_name = ProvidersNames.ADDIC7ED
+    language_to_addic7ed_code = {
+        Languages.HEBREW    : 23,
+        Languages.ENGLISH   : 1,
+        Languages.SPANISH   : 4,
+        Languages.ARABIC    : 38,
+        Languages.BULGARIAN : 35,
+        Languages.SLOVAK    : 25,
+        Languages.TURKISH   : 16,
+        Languages.CZECH     : 14,
+        Languages.RUSSIAN   : 19,
+        Languages.NORWEGIAN : 29,
+        Languages.SWEDISH   : 18,
+        Languages.FRENCH    : 8
+    }
+    supported_languages = language_to_addic7ed_code.keys()
+
+    def __init__(self, languages, requests_manager):
+        super(Addic7edProvider, self).__init__(languages, requests_manager)
--- tests/api/providers/addic7ed/test_addic7edprovider.py
+++ tests/api/providers/addic7ed/test_addic7edprovider.py
@@ -2,7 +2,8 @@ import sys
 sys.path.append("..\\..")
 import os
 
-from api.providers.addic7ed import Addic7edProvider
+from api.providers.addic7ed import provider as addic7edprovider
+Addic7edProvider = addic7edprovider.Addic7edProvider
 from api.requestsmanager import get_manager_instance
 from api.languages import Languages
 from api.title import MovieTitle
@@ -22,9 +23,32 @@ class TestAddic7edProvider(unittest.TestCase):
         """ Simple test to verify that we get version for series. """
         raise NotImplementedError()
 
-    def test_get_titles_versions_movie(self):
-        """ Simple test to verify that we get version for movie. """
-        raise NotImplementedError()
+    def test_get_titles_versions_movie_exact(self):
+        """
+        Simple test to verify that we get versions for movie. The query
+        returns a single movie in the site.
+        """
+        title = MovieTitle("Godzilla", 2014, "tt0831387")
+        fake_version = Version(["identifier"], title)
+
+        titles_versions = self.provider.get_title_versions(title, fake_version)
+
+        # Only single title is expected
+        self.assertEqual(len(titles_versions), 1)
+        # Only single language
+        self.assertEqual(len(titles_versions[0][1]), 1)
+        # Two versions (At rank group 1)
+        versions = titles_versions[0][1][Languages.ENGLISH][1]
+        self.assertEqual(len(versions), 2)
+
+        self.assertEquals(versions[0].version_string, "BluRay_BRrip_BDrip")
+        self.assertEquals(versions[1].version_string, "WEBRiP-VAiN")
+
+        self.assertEquals(versions[0].addributes["movie_code"], "/movie/89128")
+        self.assertEquals(versions[1].addributes["movie_code"], "/movie/89128")
+
+        self.assertEquals(versions[0].addributes["version_code"], "/original/89128/4")
+        self.assertEquals(versions[1].addributes["version_code"], "/original/89128/2")
 
     def test_get_subtitle_buffer(self):
         """
