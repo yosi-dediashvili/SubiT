de77314404f7d63a0be7792b5e910fe9ca8b91a7
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2015-03-26 01:02:45 +0200

    With test method. No real tests yet.

--- tests/api/providers/torec/test_torecprovider.py
+++ tests/api/providers/torec/test_torecprovider.py
@@ -19,13 +19,30 @@ class TestTorecProvider(unittest.TestCase):
         self.provider = TorecProvider(
             [Languages.HEBREW], RequestsManager())
 
+    def test_get_title_versions_multiple_results(self):
+        """ Make sure we parse the search results. """
+        pass
+
+    def test_get_title_versions_single_movie(self):
+        """ Make sure that we handle the redirection successfully. """
+        pass    
+
+    def test_get_title_versions_series(self):
+        """ Make sure we get the episodes correctly. """
+
+    def test_download_subtitle_buffer(self):
+        """ Make sure we download a file correctly. """
+        pass
+
 
 def run_tests():
     test_runner = unittest.TextTestRunner(verbosity=0)
-    tests = doctest.DocTestSuite(
-        torecprovider, 
-        optionflags=doctest.NORMALIZE_WHITESPACE)
-    tests.addTests(
-        unittest.defaultTestLoader.loadTestsFromTestCase(
-            TestTorecProvider))
+    # tests = doctest.DocTestSuite(
+    #     torecprovider, 
+    #     optionflags=doctest.NORMALIZE_WHITESPACE)
+    # tests.addTests(
+    #     unittest.defaultTestLoader.loadTestsFromTestCase(
+    #         TestTorecProvider))
+
+    tests = unittest.TestLoader().loadTestsFromTestCase(TestTorecProvider)
     test_runner.run(tests)
