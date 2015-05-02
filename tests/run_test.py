467b82d317288f67d64dc2246058e633c34c5658
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2015-05-02 18:49:41 +0300

    Changed the run_test.py file so that it uses os.path.join in order to construct the whole path of the tests. And also, chaned the imports.

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,11 +1,12 @@
 import sys
-from os.path import *
-from os import getcwd, chdir
+import os
 import argparse
 import logging
 
 logging.disable(logging.CRITICAL)
-sys.path.append(join(abspath(dirname(__file__)), r"..\src"))
+sys.path.append(
+    os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "..", "src"))
 
 parser = argparse.ArgumentParser(
     description="Executes one or more test files.")
@@ -18,18 +19,18 @@ args = parser.parse_args()
 def run_test(test_path):
     print "Running test file: %s" % test_path
     import imp
-    global_wd = getcwd()
-    dir_name = dirname(test_path)
+    global_wd = os.getcwd()
+    dir_name = os.path.dirname(test_path)
     sys.path.append(dir_name)
-    chdir(dir_name)
+    os.chdir(dir_name)
     imp.load_source('current_test', test_path).run_tests()
     sys.path.remove(dir_name)
-    chdir(global_wd)
+    os.chdir(global_wd)
 
 for test_path in args.test_paths:
-    if not isabs(test_path):
-        test_path = abspath(join(getcwd(), test_path))
-    if not exists(test_path):
+    if not os.path.isabs(test_path):
+        test_path = os.path.abspath(os.path.join(os.getcwd(), test_path))
+    if not os.path.exists(test_path):
         raise ValueError("The test file is missing: %s" % test_path)
     
     run_test(test_path)
